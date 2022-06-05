ca433855f17e00a8b10cdee1bd5b799551f20150
__p r i v a t e !! <73804955+drollgames@users.noreply.github.com>
2022-06-05 20:51:37 -0300

    Create troja.ml

---
+++ site/troja.ml
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="">
+ 
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Troja</title>
+    <link href="https://cdn.troja.ml/team/css-troja-ml-index/style.css" rel="stylesheet">
+</head>
+ 
+<body class="index-body">
+    <div class="index-header">Troja Team</div>
+       
+</body>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Staatliches&display=swap');
+</style>
+
+ 
+</html>
